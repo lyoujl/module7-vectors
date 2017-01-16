f1fc8adee7684dc629e60869b00f2f8f3bffb5c5
James Lyou <lyou.james@gmail.com>
2017-01-15 19:13:49 -0800

    Module 7 exercise 1 partially complete

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,16 +1,17 @@
 # Exercise 1: Creating and Indexing Vectors
 
 # Create a vector `x` that has the values 10 through 20 in it (using the : operator)
-
+x <- 10:20
 
 # Create a vector `y` that has the values 21 through 30 in it (using the seq() function)
-
+y <- seq(21, 30)
 
 # Create a vector `z` by combining the vectors `x` and `y`
-
+z <- c(x, y)
 
 # Create a variable `w` that contains the 11th element in your vector `z`
-
+w <- z[11]
+print(w)
 
 # Create a vector `q` that contains the second through the 5 elements of `z`
 
